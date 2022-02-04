c8b42dce67643fac48c8caff9935d26d2ee4ec47
sandy <sandy.theodorus@gmail.com>
2022-02-04 22:55:12 +0700

    style nav button

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const topNav = document.createDocumentFragment();
 
 const btnGit = document.createElement('button');
 btnGit.textContent = 'Git';
-btnGit.setAttribute('class', 'git btn-nav');
+btnGit.setAttribute('class', 'btn-nav git');
 btnGit.setAttribute('onclick', 'gitClick()');
 
 const btnHtml = document.createElement('button');
@@ -11,8 +11,14 @@ btnHtml.textContent = 'Html'
 btnHtml.className = 'html btn-nav';
 btnHtml.setAttribute('onclick', 'htmlClick()');
 
+const btnSomething = document.createElement('button');
+btnSomething.textContent = '.....................'
+btnSomething.className = 'something btn-nav';
+btnSomething.setAttribute('onclick', 'somethingClick()');
+
 topNav.appendChild(btnGit);
 topNav.appendChild(btnHtml);
+topNav.appendChild(btnSomething);
 
 document.querySelector('header').appendChild(topNav);
 // end - top nav fragment
@@ -21,17 +27,20 @@ document.querySelector('header').appendChild(topNav);
 function gitClick() {
     const gitPage = document.getElementById('git-content-page');
     gitPage.style.display = 'block';    
+    btnGit.style.backgroundColor = 'lightblue';
 
     const htmlPage = document.getElementById('html-content-page');
     htmlPage.style.display = 'none';       
-    
+    btnHtml.style.backgroundColor = 'white';
 }
 
 // show html page
 function htmlClick() {
     const gitPage = document.getElementById('git-content-page');
     gitPage.style.display = 'none';    
+    btnGit.style.backgroundColor = 'white';
 
     const htmlPage = document.getElementById('html-content-page');
     htmlPage.style.display = 'block';    
+    btnHtml.style.backgroundColor = 'lightblue';
 }
